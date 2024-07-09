0c7390881d2e7a936eab94c76cbe5ed68f8f10e3
ao627515 <ao627515@gmail.com>
2024-07-09 08:58:03 +0000

    creation dela calsse Player + les methode initialize et show_state

---
+++ lib/player.rb
@@ -0,0 +1,15 @@
+class Player
+  # accesseur
+  attr_accessor :name, :life_points
+
+  def initialize(name)
+    @name = name
+    @life_points = 10
+  end
+
+  def show_state
+    puts "#{@name} a #{@life_points} points de vie"
+  end
+
+  
+end
