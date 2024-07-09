ee60ae1fd224f5785a7c4b0780b18c1804b567db
ao627515 <ao627515@gmail.com>
2024-07-09 09:47:07 +0000

    simulation de combat

--- app.rb
+++ app.rb
@@ -8,8 +8,24 @@ require_relative 'lib/game'
 
 require_relative 'lib/player'
 
-p1 = Player.new('José')
-p2 = Player.new('Josiane')
+josiane = Player.new('Josiane')
+jose = Player.new('José')
+
+puts "Voici l'état de chaque joueur :"
+
+josiane.show_state
+jose.show_state
+
+puts "Passons à la phase d'attaque :"
+
+while jose.life_points.positive? || josiane.life_points.positive?
+  josiane.attacks(jose)
+  break if jose.life_points.zero?
+
+  puts '----------------------'
+  jose.attacks(josiane)
+  puts '----------------------'
+end
 
 
-binding.pry
+# binding.pry
