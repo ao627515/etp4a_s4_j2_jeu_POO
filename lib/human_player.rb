1630415ed7a17360d0114fa58dc05d18e2c414ee
ao627515 <ao627515@gmail.com>
2024-07-09 10:01:32 +0000

    methode initialize et redefinition de show_state

---
+++ lib/human_player.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative 'player'
+
+class HumanPlayer < Player # rubocop:disable Style/Documentation
+  attr_accessor :weapon_level
+
+  def initialize(name)
+    super(name)
+    @life_points = 100
+    @weapon_level = 1
+  end
+
+  def show_state
+    puts "#{@name} a #{@life_points} points de vie et une arme de niveau #{@weapon_level}"
+  end
+end
